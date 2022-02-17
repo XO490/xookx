69c0cac5e8fd5b9e33ec0d074841a28962128058
XO490 <xo490@protonmail.com>
2022-02-17 22:38:38 +0700

    v 0.1.3 build 71 | add build autoincrement and config.yaml

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/hajimehoshi/oto"
+	"github.com/spf13/viper"
 	"github.com/tosone/minimp3"
 	"io/ioutil"
 	"net/http"
@@ -14,9 +15,52 @@ import (
 	"time"
 )
 
-var baseUrl = "https://www.okex.com"
+type Config struct {
+	App struct {
+		debug bool
+	}
+	Headers struct {
+		head        string
+		contentType string
+		userAgent   string
+	}
+}
 
-const VERSION = "0.1.2"
+var CONFIG = Config{}
+var DEBUG = true
+var PWD string
+
+func readConfig() {
+	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	checkErr(err, DEBUG)
+	PWD = pwd
+
+	y := viper.New()
+	y.SetConfigFile(PWD + "/config.yaml")
+	err = y.ReadInConfig()
+	if err != nil {
+		createConfig()
+	}
+
+	CONFIG.Headers.head = y.GetString("headers.head")
+	CONFIG.Headers.contentType = y.GetString("headers.content-type")
+	CONFIG.Headers.userAgent = y.GetString("headers.user-agent")
+	CONFIG.App.debug = y.GetBool("app.debug")
+	DEBUG = CONFIG.App.debug
+}
+
+func createConfig() {
+	data := "app:\n" +
+		"  debug: false\n\n" +
+		"headers:\n" +
+		"  head: https://www.okex.com\n" +
+		"  content-type: application/json\n" +
+		"  user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.88 Safari/537.36 Vivaldi/5.1.2567.39\n"
+
+	err := ioutil.WriteFile(PWD+"/config.yaml", []byte(data), 0755)
+	checkErr(err, DEBUG)
+	fmt.Println("Config created in path: ", PWD)
+}
 
 func checkErr(err error, debug bool) {
 	if err != nil {
@@ -29,29 +73,47 @@ func checkErr(err error, debug bool) {
 	}
 }
 
-//https://www.okex.com/api/v5/market/index-components?index=BTC-USDT
-func apiGetCurrency(index string) string {
-	var url = baseUrl + "/api/v5/market/index-components?index=" + index
-	//println(url)
-	req, err := http.Get(url)
-	checkErr(err, false)
+func getNumOfBuild() string {
+	//pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	//checkErr(err, DEBUG)
 
-	req.Header.Add("host", baseUrl)
-	req.Header.Add("content-type", "application/json")
-	req.Header.Add("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.175 Safari/537.36")
+	build, err := os.ReadFile(PWD + "/.build")
+	if err != nil {
+		err := os.WriteFile(PWD+"/.build", []byte("0"), 0755)
+		checkErr(err, DEBUG)
+
+		build, err = os.ReadFile(PWD + "/.build")
+		checkErr(err, DEBUG)
+	}
+
+	oldbuild, _ := strconv.Atoi(string(build))
+	newbuild := strconv.Itoa(oldbuild + 1)
+
+	err = os.WriteFile(PWD+"/.build", []byte(newbuild), 0755)
+	checkErr(err, DEBUG)
+	return newbuild
+}
+
+func apiGetCurrency(index string) string {
+	/*
+		example url: https://www.okex.com/api/v5/market/index-components?index=BTC-USDT
+		api documentation: https://www.okx.com/docs-v5/en/#rest-api-market-data-get-index-components
+	*/
+	var url = CONFIG.Headers.head + "/api/v5/market/index-components?index=" + index
+	req, err := http.Get(url)
+	checkErr(err, DEBUG)
+
+	req.Header.Add("host", CONFIG.Headers.head)
+	req.Header.Add("content-type", CONFIG.Headers.contentType)
+	req.Header.Add("user-agent", CONFIG.Headers.userAgent)
 
 	var jsonObj map[string]interface{}
 	err = json.NewDecoder(req.Body).Decode(&jsonObj)
 	checkErr(err, false)
 
 	if jsonObj["code"] == "0" {
-		//index := jsonObj["data"].(map[string]interface{})["index"].(string)
 		lastFloat, _ := strconv.ParseFloat(jsonObj["data"].(map[string]interface{})["last"].(string), 32)
 		last := fmt.Sprintf("%.2f", lastFloat)
-
-		//fmt.Println(last)
-		//fmt.Println(index + ": " + last)
-		//fmt.Println(reflect.TypeOf(last))
 
 		return last
 	} else {
@@ -65,24 +127,25 @@ func help() {
 		"---------\n" +
 		"usage:\n" +
 		"\t--index=BTC-USDT\t| return 'last' > https://www.okx.com/docs-v5/en/#rest-api-market-data-get-index-components\n" +
-		"\t--sound\t\t\t| default sound notification (use --sound OR --custom-sound)\n" +
+		"\t--sound\t\t\t| default sound notification: --sound (use --sound OR --custom-sound)\n" +
 		"\t--custom-sound\t\t| your custom sound notification: --custom-sound=btc.mp3 (use --sound OR --custom-sound)\n" +
+		"\t--config\t\t\t| create config.yaml file\n" +
 		"\nerror-code </>:\t> https://www.okx.com/docs-v5/en/#error-code\n" +
 		"feedback:\t> https://t.me/xo490")
 }
 
 func notification(filename string) {
-	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	checkErr(err, true)
+	//pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	//checkErr(err, DEBUG)
 
-	file, err := ioutil.ReadFile(pwd + "/media/" + filename)
-	checkErr(err, true)
+	file, err := ioutil.ReadFile(PWD + "/media/" + filename)
+	checkErr(err, DEBUG)
 
 	dec, data, err := minimp3.DecodeFull(file)
-	checkErr(err, true)
+	checkErr(err, DEBUG)
 
 	context, err := oto.NewContext(dec.SampleRate, dec.Channels, 2, 1024)
-	checkErr(err, true)
+	checkErr(err, DEBUG)
 
 	var player = context.NewPlayer()
 	player.Write(data)
@@ -93,7 +156,7 @@ func notification(filename string) {
 	defer dec.Close()
 
 	err = player.Close()
-	checkErr(err, true)
+	checkErr(err, DEBUG)
 }
 
 func getArgs() {
@@ -104,6 +167,7 @@ func getArgs() {
 		index := flag.String("index", "", "--index=BTC-USDT")
 		sound := flag.Bool("sound", false, "--sound")
 		customSound := flag.String("custom-sound", "", "--custom-sound=up.mp3")
+		config := flag.Bool("config", false, "--config")
 
 		flag.Parse()
 
@@ -118,11 +182,39 @@ func getArgs() {
 		if *customSound != "" {
 			notification(*customSound)
 		}
+
+		if *config {
+			createConfig()
+		}
 	}
 }
 
-func main() {
+/*
+	Building app
+*/
+
+var VERSION string
+
+func buildApp(release bool, version string, buildNum string) {
+	readConfig()
+
+	if release {
+		if buildNum != "" {
+			VERSION = version + " | build " + buildNum + " | release"
+		} else {
+			fmt.Println("For RELEASE=true buildNum can't be empty")
+			os.Exit(1)
+		}
+	}
+
+	if release != true {
+		buildNum = getNumOfBuild()
+		VERSION = version + " | build " + getNumOfBuild() + " | debug"
+	}
+
 	getArgs()
-	//notification("up.mp3")
-	//fmt.Print(apiGetCurrency("TONCOIN-USDT"))
+}
+
+func main() {
+	buildApp(true, "0.1.3", "71")
 }
