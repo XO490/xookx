0f6ab2ffac375b70b15182c648fd0bb5c6b93a28
XO490 <xo490@protonmail.com>
2023-05-18 22:25:20 +0700

    readme.md update

---
+++ cmd/text/texts.go
@@ -0,0 +1,10 @@
+package text
+
+const (
+	Index       = "return 'last', see https://www.okx.com/docs-v5/en/#rest-api-market-data-get-index-components"
+	Sound       = "default sound notification: --sound (use --sound OR --custom-sound)"
+	CustomSound = "your custom sound notification: --custom-sound=btc.mp3 (use --sound OR --custom-sound)"
+	Config      = "create config.yaml file"
+	ErrorCode   = "https://www.okx.com/docs-v5/en/#error-code"
+	Contact     = "feedback: https://t.me/xo490"
+)
